19d44565796373ebf6f2cf23f39d9f713ff28060
DharaniMuli <46985863+DharaniMuli@users.noreply.github.com>
2019-09-20 17:52:06 -0500

    ICP5

--- ICPs/ICP-5/Python_Lesson5/remove_anamolies.py
+++ ICPs/ICP-5/Python_Lesson5/remove_anamolies.py
@@ -23,10 +23,11 @@ plt.xlabel('GarageArea')
 plt.ylabel('SalePrice')
 plt.show()
 
-# Using box plot to identifty the outliers
-sns.boxplot(x=df['GarageArea'])
+# # Using box plot to identifty the outliers
+# sns.boxplot(x=df['GarageArea'])
+# plt.show()
 
-# Removing the Anamolies by directly specifying the limits of the outliers
+# # Removing the Anamolies by directly specifying the limits of the outliers
 train = df
 train['GarageArea'] = train[train['GarageArea']>200]
 train['GarageArea'] = train[train['GarageArea']<1200]
@@ -36,63 +37,6 @@ train['GarageArea'] = train[train['GarageArea']<1200]
 sns.lmplot('GarageArea', # Horizontal axis
            'SalePrice', # Vertical axis
            data=train, # Data source
-           fit_reg=False, # Don't fix a regression line
-           scatter_kws={"marker": "o", # Set marker style
-                        "s": 80}) # S marker size
-
-# Set title
-plt.title('plot between Garage Area and SalPrice')
-plt.xlim(-200, 1600)
-# Set x-axis label
-plt.xlabel('GarageArea')
-# Set y-axis label
-plt.ylabel('SalePrice')
-plt.show()
-
-
-# Removing the Anamolies using z-score
-# if the data is more than 3 standard deviations away then it is considered as outlier
-# if the data is less than -3 standard deviations away then it is considered as outlier
-df = pd.read_csv('train.csv', sep=',',usecols=(62,80))
-z = np.abs(stats.zscore(df))
-threshold = 3
-print(np.where(z > 3))
-modified_df = df[(z < 3).all(axis=1)]
-print(df.shape)
-print(modified_df.shape)
-
-# Create Scatterplot of the dataframe after removing the anamolies in the data
-sns.lmplot('GarageArea', # Horizontal axis
-           'SalePrice', # Vertical axis
-           data=modified_df, # Data source
-           fit_reg=False, # Don't fix a regression line
-           scatter_kws={"marker": "o", # Set marker style
-                        "s": 80}) # S marker size
-
-# Set title
-plt.title('plot between Garage Area and SalPrice')
-plt.xlim(-200, 1600)
-# Set x-axis label
-plt.xlabel('GarageArea')
-# Set y-axis label
-plt.ylabel('SalePrice')
-plt.show()
-
-
-# Removing the anamolies using Quantile - removing less than 25% and more than 75% Qunatile
-Q1 = df.quantile(0.25)
-Q3 = df.quantile(0.75)
-IQR = Q3 - Q1
-print(IQR)
-print((df < (Q1 - 1.5 * IQR)) |(df > (Q3 + 1.5 * IQR)))
-
-modified_df = df[~((df < (Q1 - 1.5 * IQR)) |(df > (Q3 + 1.5 * IQR))).any(axis=1)]
-print(modified_df.shape)
-
-# Create Scatterplot of the dataframe after removing the anamolies in the data
-sns.lmplot('GarageArea', # Horizontal axis
-           'SalePrice', # Vertical axis
-           data=modified_df, # Data source
            fit_reg=False, # Don't fix a regression line
            scatter_kws={"marker": "o", # Set marker style
                         "s": 80}) # S marker size
