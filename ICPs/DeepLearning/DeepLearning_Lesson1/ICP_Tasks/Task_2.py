a46b903663f684e030dffc2d43547789c7c605d8
DharaniMuli <46985863+DharaniMuli@users.noreply.github.com>
2019-10-18 17:33:05 -0500

    ICP1

--- ICPs/DeepLearning/DeepLearning_Lesson1/ICP_Tasks/Task_2.py
+++ ICPs/DeepLearning/DeepLearning_Lesson1/ICP_Tasks/Task_2.py
@@ -1,32 +1,40 @@
-import warnings
-warnings.filterwarnings("ignore")
+import pandas
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# load dataset
 import pandas as pd
+dataset = pd.read_csv("../Breast_Cancer.csv")
+# print(dataset)
+
+classes = ['Benign', 'Malignant']
+
+x = dataset.iloc[:, 2:32].values
+y = dataset.iloc[:, 1].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder
+labelencoder_X_1 = LabelEncoder()
+y = labelencoder_X_1.fit_transform(y)
+# print(y)
 
 from sklearn.model_selection import train_test_split
-dataset = pd.read_csv("../Breas Cancer.csv")
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
 
-x1=dataset.drop(['diagnosis', 'id', 'fractal_dimension_worst'],axis=1)
-y1=dataset['diagnosis']
+# from sklearn.preprocessing import StandardScaler
+# # sc = StandardScaler()
+# # X_train = sc.fit_transform(X_train)
+# # X_test = sc.transform(X_test)
 
-y1 = y1.map({'B': 1, 'M':1})
-
-import numpy as np
-X_train, X_test, Y_train, Y_test = train_test_split(x1, y1, test_size=0.25, random_state=87)
-np.random.seed(155)
 my_first_nn = Sequential() # create model
-my_first_nn.add(Dense(50, input_dim=30, activation='sigmoid')) # hidden layer
-my_first_nn.add(Dense(25, activation='sigmoid')) # hidden layer
+my_first_nn.add(Dense(8, input_dim=30, activation='relu')) # hidden layer
+my_first_nn.add(Dense(20, activation='relu')) # hidden layer
+my_first_nn.add(Dense(50, activation='relu')) # hidden layer
 my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
-my_first_nn.compile(loss='binary_crossentropy', optimizer='adam')
-my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100,verbose=0,
+my_first_nn.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+my_first_nn_fitted = my_first_nn.fit(X_train, y_train, epochs=100, verbose=0,
                                      initial_epoch=0)
 print(my_first_nn.summary())
-print(my_first_nn.evaluate(X_test, Y_test,verbose=0))
-
-"""
-Accuracy is increasing to a certain point when we add layers. 
-If we add more number of layers then slowly the accuracy is decreasing
-If random seed is given then -> the accuracy doesnot change as seed takes care of setting same rows for every iteration
-"""
+print(my_first_nn.evaluate(X_test, y_test, verbose=0))
